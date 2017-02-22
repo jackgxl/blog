c751b5f40d1e1909477a28993f051e09a379aac0
jack Gao <13716361153@163.com>
2017-02-22 15:41:28 +0800

    13

---
+++ py_sunday/demo/10.py
@@ -0,0 +1,8 @@
+# _*_ coding:utf8 _*_
+import time
+print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+time.sleep(1)
+
+
+print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
