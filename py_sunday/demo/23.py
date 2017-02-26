c058e3b64635de2c004c7b28d6ec00a39f83e01a
jack Gao <13716361153@163.com>
2017-02-26 16:37:55 +0800

    23-2

--- py_sunday/demo/23.py
+++ py_sunday/demo/23.py
@@ -6,3 +6,10 @@ for i in range(4):
     for k in range(2*i+1):
         stdout.write('*')
     print
+
+for i in range(3):
+    for j in range(i+1):
+        stdout.write(' ')
+    for k in range(4-2*i+1):
+        stdout.write('*')
+    print
