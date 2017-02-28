ab08dba8e1df91195f0655b5cf1a738b5847f036
jack Gao <13716361153@163.com>
2017-02-28 17:38:05 +0800

    27-2

--- py_sunday/demo/27.py
+++ py_sunday/demo/27.py
@@ -1,9 +1,10 @@
 # _*_ coding:utf8 _*_
-l=[]
-for i in range(1,6):
-    s = raw_input("please input a character \n")
-    l = l.append(s)
-    print l
-for j in range(0,len(l)+1,-1):
-    print l[j]
+def output(s,l):
+    if l == 0:
+        return 0
+    print (s[l-1])
+    output(s,l-1)
 
+s = raw_input('Input a string: \n')
+l = len(s)
+output(s,l)
