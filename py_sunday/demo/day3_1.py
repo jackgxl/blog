6dc3f86d0c787f1b4934b0e073e85f85ab30ed70
jackgxl <13716361153@163.com>
2017-03-05 17:43:12 +0800

    day3

---
+++ py_sunday/demo/day3_1.py
@@ -0,0 +1,4 @@
+for i in range(1,26):
+    for j in range(1,20):
+        if 4*i + 6*j == 100:
+            print(i,j)
---
+++ py_sunday/demo/day3_3.py
@@ -0,0 +1,1 @@
+a=[(x,y) for x in range(range(1,100//4+1) for y in range(1,100//6+1)]
