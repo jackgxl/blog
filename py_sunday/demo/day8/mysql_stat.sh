e7eb97f19c8bad6022fe39950f0db604318da4c6
jack Gao <13716361153@163.com>
2017-04-16 10:32:01 +0800

    day9

--- py_sunday/demo/day8/mysql_stat.sh
+++ py_sunday/demo/day8/mysql_stat.sh
@@ -2,5 +2,12 @@
 # -*- coding:utf8 -*-
 #author:gaoxueliang
 #document:mysql status.
-import pymysql
-
+import mysql.connector
+conn = mysql.connector.connect(host='localhost',port=3306,user='root',password='213456',database='db1')
+cursor = conn.cursor()
+cursor.execute('show global status;')
+data = cursor.fetchall()
+print(data)
+conn.commit()
+cursor.close()
+conn.close()
---
+++ py_sunday/demo/day9/day9.md
@@ -0,0 +1,22 @@
+# day 9 
+## web
+
+### 回顾
+**高内聚，低耦合**
+
+``` python
+import time
+while True:
+    print(1)
+    time.sleep(10)
+```
+
+```
+def _get_threads()
+def _get_slave_info()
+def _get_other()
+def _execute()
+def _open_file()
+def _log()
+```
+
